405600c7120b218b0925a1e6d34a3ef997fc9e0a
Daniel <urbandanielsp@gmail.com>
2026-03-18 17:32:43 -0300

    new spring boot project - first commit

---
+++ easy-offers/src/main/java/com/easy/offers/EasyOffersApplication.java
@@ -0,0 +1,13 @@
+package com.easy.offers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyOffersApplication.class, args);
+	}
+
+}
---
+++ easy-offers/src/test/java/com/easy/offers/EasyOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.offers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
